64bc21e4c32cd4aa681d3459ebf03fd2db47f2c5
Nermeeta Dhillon <nermeetadhillon@Nermeetas-MBP.attlocal.net>
2022-02-03 18:18:14 -0800

    Changed JSON code

--- sdhealthdisparities/node_adjust.py
+++ sdhealthdisparities/node_adjust.py
@@ -1,5 +1,14 @@
 # coding: utf-8
-import os
+with open('data.json') as json_file:
+    data = json.load(json_file)
+for dictionaries in data['nodes']:
+    if 'Research Cluster' in dictionaries['label']:
+        size_ = {'size': 10.0 }
+        dictionaries.update(size_)
+    else:
+        size__ = {'size': 1.0 }
+        dictionaries.update(size__)
+get_ipython().run_line_magic('save', "'node_adjust'  1-5")
 import json
 with open('data.json') as json_file:
     data = json.load(json_file)
@@ -7,5 +16,7 @@ for dictionaries in data['nodes']:
     if 'Research Cluster' in dictionaries['label']:
         size_ = {'size': 10.0 }
         dictionaries.update(size_)
-json.dump(data,open('data.json','w'))
-get_ipython().run_line_magic('save', "'node_adjust'  1-5") 
+    else:
+        size__ = {'size': 1.0 }
+        dictionaries.update(size__)
+get_ipython().run_line_magic('save', "'node_adjust'  1-5")
