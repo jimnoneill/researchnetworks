e2aaabb04d334c8ee5ceab61123e4bbbf42dd570
Nermeeta Dhillon <nermeetadhillon@Nermeetas-MBP.attlocal.net>
2022-02-03 18:24:15 -0800

    JSON updat

--- sdhealthdisparities/node_adjust.py
+++ sdhealthdisparities/node_adjust.py
@@ -1,14 +1,4 @@
 # coding: utf-8
-with open('data.json') as json_file:
-    data = json.load(json_file)
-for dictionaries in data['nodes']:
-    if 'Research Cluster' in dictionaries['label']:
-        size_ = {'size': 10.0 }
-        dictionaries.update(size_)
-    else:
-        size__ = {'size': 1.0 }
-        dictionaries.update(size__)
-get_ipython().run_line_magic('save', "'node_adjust'  1-5")
 import json
 with open('data.json') as json_file:
     data = json.load(json_file)
@@ -19,4 +9,5 @@ for dictionaries in data['nodes']:
     else:
         size__ = {'size': 1.0 }
         dictionaries.update(size__)
+json.dump(data,open('data.json','w'))
 get_ipython().run_line_magic('save', "'node_adjust'  1-5")
