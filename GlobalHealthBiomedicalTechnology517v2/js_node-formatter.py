015c50bcd38fd9680e1cbbe5aad8210bf61aaed6
Nermeeta Dhillon <nermeetadhillon@Nermeetas-MacBook-Pro.local>
2022-05-17 09:54:44 -0700

    Global Health Disparity and Biomedical Tehnology Map 517v2

--- GlobalHealthBiomedicalTechnology517v2/js_node-formatter.py
+++ GlobalHealthBiomedicalTechnology517v2/js_node-formatter.py
@@ -5,7 +5,6 @@ with open('data.json') as json_file:
     for dictionaries in data['nodes']:
         if 'Biomedical Technology Research Cluster ' in dictionaries['label']:
             size_ = {'size': 3.0 }
-            color_ = { 'color' : 'rgb(153,255,153)' }
             dictionaries.update(size_)
             #dictionaries.update(color_)
         elif 'Global Health Disparity Research Cluster ' in dictionaries['label']:
