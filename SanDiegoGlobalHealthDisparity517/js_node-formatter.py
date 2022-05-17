315769f71330b9427f034f566c5e35c63d63575a
Nermeeta Dhillon <nermeetadhillon@Nermeetas-MBP.attlocal.net>
2022-05-17 07:03:16 -0700

    San Diego Health Disparity and Global Health Disparity Map 517

---
+++ SanDiegoGlobalHealthDisparity517/js_node-formatter.py
@@ -0,0 +1,19 @@
+import json
+
+with open('data.json') as json_file:
+    data = json.load(json_file)
+    for dictionaries in data['nodes']:
+        if 'Global Health Disparity Research Cluster ' in dictionaries['label']:
+            size_ = {'size': 3.0 }
+            color_ = { 'color' : 'rgb(153,255,153)' }
+            dictionaries.update(size_)
+            #dictionaries.update(color_)
+        elif 'San Diego Health Disparity Research Cluster ' in dictionaries['label']:
+            size_ = {'size': 3.0 }
+            color_ = { 'color' : 'rgb(153,255,153)' }
+            dictionaries.update(size_)
+            dictionaries.update(color_)
+        else:
+            size__ = {'size' : 1.0}
+            dictionaries.update(size__)
+json.dump(data,open('data.json','w'))
