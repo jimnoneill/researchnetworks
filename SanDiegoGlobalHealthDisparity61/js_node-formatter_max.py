232f5e1aa7cf38e09a7df9dcdafeb372ef6f2f8e
jimnoneill <jnoneill@ucdavis.ed>
2022-06-01 17:30:31 -0700

    testing gradient maker

--- SanDiegoGlobalHealthDisparity61/js_node-formatter_max.py
+++ SanDiegoGlobalHealthDisparity61/js_node-formatter_max.py
@@ -7,7 +7,7 @@ label_id = {}
 id_label = {}
 data = json.load(open('data.json','r'))
 json.dump(data,open('data.json_bak','w'))
-for dictionary in data['nodes']:
+for c,dictionary in enumerate(data['nodes']):
     if dictionary['label'] in ghdbiomed:
         label_id[dictionary['label']] = dictionary['id']
         id_label[dictionary['id']] = dictionary['label']
@@ -15,22 +15,22 @@ for dictionary in data['nodes']:
         color_[1] = '255'
         color = ",".join(color_)
         color_change = { 'color' : str(color) }
-        dictionary.update(color_change)
-for dictionary in data['edges']:
+        data['nodes'][c].update(color_change)
+for c,dictionary in enumerate(data['edges']):
     if dictionary['source'] in list(id_label.keys()) or dictionary['target'] in list(id_label.keys()):
-        
+        print(True)
         color_ = dictionary['color'].split(',')
         color_[1] = '255'
         color = ",".join(color_)
         color_change = { 'color' : color }
-        dictionary.update(color_change)
+        data['edges'][c].update(color_change)
     elif dictionary['source'] in list(id_label.keys()) or dictionary['target'] in list(id_label.keys()):
         color_ = dictionary['color'].split(',')
         color_change = str(int(np.mean([255,int(color_[1])])))
         color_[1] = color_change
         color = ",".join(color_)
         color_change = { 'color' : color }
-        dictionary.update(color_change)
+        data['edges'][c].update(color_change)
         
 
                 
