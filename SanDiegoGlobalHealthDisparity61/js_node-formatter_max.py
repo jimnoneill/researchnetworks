2c0e23f6207ed50bda3bb2e7165d185849345a8d
jimnoneill <jnoneill@ucdavis.ed>
2022-06-01 17:24:28 -0700

    testing gradient maker

--- SanDiegoGlobalHealthDisparity61/js_node-formatter_max.py
+++ SanDiegoGlobalHealthDisparity61/js_node-formatter_max.py
@@ -17,20 +17,20 @@ for dictionary in data['nodes']:
         color_change = { 'color' : str(color) }
         dictionary.update(color_change)
 for dictionary in data['edges']:
-    if dictionary['source'] in list(id_label.values()) or dictionary['target'] in list(id_label.values()):
+    if dictionary['source'] in list(id_label.keys()) or dictionary['target'] in list(id_label.keys()):
         
         color_ = dictionary['color'].split(',')
         color_[1] = '255'
         color = ",".join(color_)
         color_change = { 'color' : color }
         dictionary.update(color_change)
-    #elif dictionary['source'] in list(id_label.values()) or dictionary['target'] in list(id_label.values()):
-        #color_ = dictionary['color'].split(',')
-        #color_change = str(int(np.mean([255,int(color_[1])])))
-        #color_[1] = color_change
-        #color = ",".join(color_)
-        #color_change = { 'color' : color }
-        #dictionary.update(color_change)
+    elif dictionary['source'] in list(id_label.keys()) or dictionary['target'] in list(id_label.keys()):
+        color_ = dictionary['color'].split(',')
+        color_change = str(int(np.mean([255,int(color_[1])])))
+        color_[1] = color_change
+        color = ",".join(color_)
+        color_change = { 'color' : color }
+        dictionary.update(color_change)
         
 
                 
