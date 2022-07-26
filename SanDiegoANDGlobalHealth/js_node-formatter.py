ea8639f3bdb21f2228fb4baa18b94792ce855ab1
Nermeeta Dhillon <nermeetadhillon@Nermeetas-MBP.attlocal.net>
2022-07-26 13:13:26 -0700

    San Diegoand Global Health Disparities Map

--- SanDiegoANDGlobalHealth/js_node-formatter.py
+++ SanDiegoANDGlobalHealth/js_node-formatter.py
@@ -52,35 +52,5 @@ for c,dictionary in enumerate(data['edges']):
             #print(color_change)
             data['edges'][c].update(color_change)
 
-ghdbiomed= pickle.load(open('ListofSDTokensOverlay.pickle','rb')) 
-label_id = {}
-id_label = {}
-data = json.load(open('data.json','r'))
-json.dump(data,open('data.json_bak','w'))
-for c,dictionary in enumerate(data['nodes']):
-    if dictionary['label'] in ghdbiomed:
-        label_id[dictionary['label']] = dictionary['id']
-        id_label[dictionary['id']] = dictionary['label']
-        color_ = dictionary['color'].split(',')
-        color_[1] = '255'
-        color = ",".join(color_)
-        color_change = { 'color' : str(color) }
-        data['nodes'][c].update(color_change)
-for c,dictionary in enumerate(data['edges']):
-    if dictionary['source'] in list(id_label.keys()) and dictionary['target'] in list(id_label.keys()):
-        print(True)
-        color_ = dictionary['color'].split(',')
-        color_[1] = '255'
-        color = ",".join(color_)
-        color_change = { 'color' : str(color) }
-        data['edges'][c].update(color_change)
-    elif dictionary['source'] in list(id_label.keys()) or dictionary['target'] in list(id_label.keys()):
-        color_ = dictionary['color'].split(',')
-        color_change = str(int(np.mean([255,int(color_[1])])))
-        color_[1] = color_change
-        color = ",".join(color_)
-        color_change = { 'color' : str(color) }
-        data['edges'][c].update(color_change)
-
 
 json.dump(data,open('data.json','w'))
