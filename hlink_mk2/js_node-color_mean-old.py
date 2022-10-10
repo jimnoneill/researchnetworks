e3870b381b9dbdd08b27f22370edb880f361e4bf
jimnoneill <jnoneill@ucdavis.ed>
2022-10-10 16:52:31 -0700

    hlink mk2 concrete filter and redundancy collapse modules add

---
+++ hlink_mk2/js_node-color_mean-old.py
@@ -0,0 +1,33 @@
+import json
+import pickle
+import numpy as np
+# take nodes and only
+#ghdbiomed= pickle.load(open('SanDiegoHDtokens.pickle','rb')) 
+#label_id = {}
+#id_label = {}
+#data = json.load(open('data.json','r'))
+#json.dump(data,open('data.json_bak','w'))
+#for c,dictionary in enumerate(data['nodes']):
+    #if dictionary['label'] in ghdbiomed:
+        #label_id[dictionary['label']] = dictionary['id']
+        #id_label[dictionary['id']] = dictionary['label']
+        #color_ = dictionary['color'].split(',')
+        #color_[1] = '255'
+        #color = ",".join(color_)
+        #color_change = { 'color' : str(color) }
+        #data['nodes'][c].update(color_change)
+#for c,dictionary in enumerate(data['edges']):
+    #if dictionary['source'] in list(id_label.keys()) and dictionary['target'] in list(id_label.keys()):
+        #print(True)
+        #color_ = dictionary['color'].split(',')
+        #color_[1] = '255'
+        #color = ",".join(color_)
+        #color_change = { 'color' : str(color) }
+        #data['edges'][c].update(color_change)
+    #elif dictionary['source'] in list(id_label.keys()) or dictionary['target'] in list(id_label.keys()):
+        #color_ = dictionary['color'].split(',')
+        #color_change = str(int(np.mean([255,int(color_[1])])))
+        #color_[1] = color_change
+        #color = ",".join(color_)
+        #color_change = { 'color' : color }
+        #data['edges'][c].update(color_change)
--- hlink_mk2/node_reformatteroverlay.py
+++ hlink_mk2/node_reformatteroverlay.py
@@ -15,9 +15,8 @@ hlink_tokens = pickle.load(open('/home/jimnoneill/HLINK_top_tokens.pickle','rb')
 label_id = {}
 id_label = {}
 data = json.load(open('data.json','r'))
-colors  = (0,255,0)
+colors  = (102,0,0)
 json.dump(data,open('data.json_bak','w'))
-i = range(1,10)
 
 for c,dictionary in enumerate(data['nodes']):
     if dictionary['label'] in hlink_tokens:
@@ -31,13 +30,34 @@ for c,dictionary in enumerate(data['nodes']):
         size_ = {'size': 1.0 }
         data['nodes'][c].update(size_)
         data['nodes'][c].update(color_change)
-    for i in range(10):
-        if 'HLINK Node ' + str(i+1)  in dictionary['label']:
-            colorstr = str(colors)
-            color_change = { 'color' : 'rgb'+colorstr }
-            size_ = {'size': 3.0 }
-            data['nodes'][c].update(size_)
-            data['nodes'][c].update(color_change)
+    elif dictionary['label'].startswith('HLINK'):
+        colorstr = str(colors)
+        color_change = { 'color' : 'rgb'+colorstr }
+        size_ = {'size': 3.0 }
+        data['nodes'][c].update(size_)
+        data['nodes'][c].update(color_change)
 
+sources  = []
+for c,dictionary in enumerate(data['edges']):
+    #print(c)
+    #for i in range(len(keys)):
+    s = dictionary['source']
+    sources.append(s)
+
+updatedcolors = {}
+for i in sources:
+    for key, values in idscolors.items():
+        if i == key:
+            updatedcolors[i] = idscolors.get(key)
+
+#print(updatedcolors)
+keys = list(updatedcolors.keys())
+for c,dictionary in enumerate(data['edges']):
+    for i in range(len(keys)):
+        if dictionary['source'] in keys[i]:
+            color_change = updatedcolors.get(keys[i])
+            
+            data['edges'][i].update(color_change)
+            
 
 json.dump(data,open('data.json','w'))
