2bc5edf6e6c2eeffb6de16dd46c6c39936bc755c
jimnoneill <jnoneill@ucdavis.ed>
2022-10-11 15:18:06 -0700

    node and edge format test

--- hlink_mk2/js_node-formatter.py
+++ hlink_mk2/js_node-formatter.py
@@ -5,12 +5,12 @@ import math
 import pandas as pd
 import numpy as np
 import random
-
+### new plan, 10/11/22 - load top tokens relative to groups. enumerate over groups to align them with node color changes. 
 #hlink_tokens = pickle.load(open('/home/jimnoneill/HLINK_top_tokens.pickle','rb'))
+
+group_top_sd_tokens,group_top_hd_tokens = pickle.load(open('/home/jimnoneill/group_top_sd_tokens,group_top_hd_tokens.pickle','rb'))
 ghd_tokens = pickle.load(open('/home/jimnoneill/ghd_top_tokens.pickle','rb'))
-#ghdbiomed= pickle.load(open('BiomedicalTechnology727.pickle','rb'))
-#colors = [(128,0,0),(165,42,42),(255,0,0),(255,215,0),(238,232,170),(255,255,0),(154,205,50),(127,255,0),(0,128,0),(143,188,143),(102,205,170),(47,79,79),(0,128,128),(0,255,255),(224,255,255),(64,224,208),(127,255,212),(70,130,180),(100,149,237),(0,191,255),(30,144,255),(135,206,250),(0,0,128),(0,0,255),(138,43,226),(75,0,130),(123,104,238),(139,0,139),(148,0,211),(186,85,211),(221,160,221),(238,130,238),(255,0,255),(199,21,133),(255,20,147),(255,192,203),(255,228,196),(139,69,19),(244,164,96),(188,143,143),(255,240,245),(245,255,250),(112,128,144),(240,255,240)]
-#colors = pickle.load(open('RGBColors.pickle','rb'))
+
 colors = pickle.load(open('/home/jimnoneill/rgb_colors_large.pickle','rb'))
 random.shuffle(colors)
 
@@ -23,60 +23,50 @@ tokenslist = ghd_tokens##[]
 
 data = json.load(open('data.json','r'))
 #json.dump(data,open('data.json_bak','w'))
-
+color_index = 0
 idscolors = {} 
+cluster_colors = {}
 for c,dictionary in enumerate(data['nodes']):
     if dictionary['label'].startswith('Global Health'):
+        cluster = int(dictionary['label'][-2:].strip())
         #print(dictionary)
         idf = dictionary['id']
-        colorstr = str(colors[c])
+        colorstr = str(colors[color_index])
         color_change = 'rgb'+colorstr
         #print(type(color_change))
         size_ = {'size': 3.0 }
         idscolors[idf] = color_change
         data['nodes'][c]['size'] = 3.0
         data['nodes'][c]['color'] = color_change
-###FIX make dictionary of 'label': ids ~ if "target" or "source" in idscolors.keys()
-for i in range(len(tokenslist)):
-    
-    
-    if dictionary['label'] in tokenslist[i]:
-        #FIX look up all connections~ if "target" or "source" in idscolors.keys()
+        cluster_colors[cluster] = color_change
+        color_index += 1
+
+    else:
+        continue
+for n,group in enumerate(group_top_hd_tokens):
+    n +=1
+    color_change = cluster_colors[n]
+    for c,dictionary in enumerate(data['nodes']):
+        if dictionary['label'] in group:
+            #print(dictionary)
             idf = dictionary['id']
-            #colorstr = str(colors[i])
-            color_change = 'rgb'+str(colorstr)
+            #colorstr = str(colors[color_index])
+            #color_change = 'rgb'+colorstr
+            #print(type(color_change))
             size_ = {'size': 1.0 }
-            #idscolors[idf] = color_change
+            idscolors[idf] = color_change
             data['nodes'][c]['size'] = 1.0
-            data['nodes'][c]['color'] =color_change
-            #print(color_change)
+            data['nodes'][c]['color'] = color_change
+            #color_index += 1
 
+        else:
+            continue
 
-
-
-#keys = list(idscolors.keys())
-
-#keys = list(idscolors.keys())
-sources  = []
 for c,dictionary in enumerate(data['edges']):
-    #print(c)
-    #for i in range(len(keys)):
-    s = dictionary['source']
-    sources.append(s)
-
-updatedcolors = {}
-for i in sources:
-    for key, values in idscolors.items():
-        if i == key:
-            updatedcolors[i] = idscolors.get(key)
-
-#print(updatedcolors)
-keys = list(updatedcolors.keys())
-for c,dictionary in enumerate(data['edges']):
-    for i in range(len(keys)):
-        if dictionary['source'] in keys:
-            color_change = updatedcolors[dictionary['source']]#.get(keys[i])
-            data['edges'][i]['color'] = color_change
+    for ids,colors in idscolors.items():
+        if dictionary['source'] == ids:
+            data['edges'][c]['color'] = colors
+            
             
 
 
