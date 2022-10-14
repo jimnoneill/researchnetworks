814417cf9d147fe36401db803036f1b8004f954d
jimnoneill <jnoneill@ucdavis.ed>
2022-10-14 16:45:23 -0700

    add edge highlight to Hlink map

---
+++ GlobalHealth_HLinkMap_colors_highlights/node_edges_reformatteroverlay.py
@@ -0,0 +1,40 @@
+import json
+import pickle5 as pickle
+from collections import Counter
+import math
+import pandas as pd
+import numpy as np
+import random
+
+
+data = json.load(open('data.json','r'))
+json.dump(data,open('data.json_bak','w'))
+
+ghdbiomed= pickle.load(open('ListofGlobalHealthTokensOverlay.pickle','rb')) 
+
+label_id = {}
+id_label = {}
+data = json.load(open('data.json','r'))
+colors  = (0,0,188)
+json.dump(data,open('data.json_bak','w'))
+i = range(1,10)
+
+for c,dictionary in enumerate(data['nodes']):
+    if dictionary['label'] in ghdbiomed:
+        label_id[dictionary['label']] = dictionary['id']
+        id_label[dictionary['id']] = dictionary['label']
+        #color_ = dictionary['color'].split(',')
+        #color_[1] = '255'
+        #color = ",".join(color_)
+
+
+
+#print(updatedcolors)
+keys = list(id_label.keys())
+for c,dictionary in enumerate(data['edges']):
+    for i in range(len(keys)):
+        if dictionary['source'] in keys[i]:
+            size_update = data['edges'][i]['size']*5
+            data['edges'][i]['size'] = size_update
+            
+json.dump(data,open('data.json','w'))
