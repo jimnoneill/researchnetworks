0895d067b9b4072f670975c72fff8a8d22f7bfad
jimnoneill <jnoneill@ucdavis.ed>
2022-10-14 19:38:55 -0700

    edges attempt closte but so far

--- GlobalHealth_HLinkMap_colors_highlights/node_edges_reformatteroverlay.py
+++ GlobalHealth_HLinkMap_colors_highlights/node_edges_reformatteroverlay.py
@@ -18,7 +18,6 @@ data = json.load(open('data.json','r'))
 
 json.dump(data,open('data.json_bak','w'))
 i = range(1,10)
-
 for c,dictionary in enumerate(data['nodes']):
     if dictionary['label'] in ghdbiomed:
         label_id[dictionary['label']] = dictionary['id']
@@ -27,20 +26,23 @@ for c,dictionary in enumerate(data['nodes']):
         #color_[1] = '255'
         #color = ",".join(color_)
 
+    for i in range(10):
+        if 'Health Link Research Cluster ' + str(i+1)  in dictionary['label']:
+            label_id[dictionary['label']] = dictionary['id']
+            id_label[dictionary['id']] = dictionary['label']
 
 
 #print(updatedcolors)
 keys = list(id_label.keys())
 for c,dictionary in enumerate(data['edges']):
-    for i in range(len(keys)):
-        if dictionary['source'] in keys[i]:
-            print(True)
-            ###size_update = data['edges'][c]['size']*25
-            data['edges'][c]['size'] = 10.0
-            print(data['edges'][c]['size'])
-        else:
-            data['edges'][c]['size'] = 1.0
-            print(data['edges'][c]['size'])
+    if dictionary['source'] in keys:
+        print(True)
+        ###size_update = data['edges'][c]['size']*25
+        data['edges'][c]['size'] = 10.0
+        print(data['edges'][c]['size'])
+    else:
+        continue
+
 #for c,dictionary in enumerate(data['edges']):
     #for i in range(len(keys)):
         #if dictionary['target'] not in keys[i]:
