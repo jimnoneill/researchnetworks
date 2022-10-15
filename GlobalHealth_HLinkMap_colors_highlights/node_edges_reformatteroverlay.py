a2e2aa99ab6d6761857a567b747e61e4c2307cc0
jimnoneill <jnoneill@ucdavis.ed>
2022-10-14 18:58:33 -0700

    pastel add and new overlay color

--- GlobalHealth_HLinkMap_colors_highlights/node_edges_reformatteroverlay.py
+++ GlobalHealth_HLinkMap_colors_highlights/node_edges_reformatteroverlay.py
@@ -30,21 +30,23 @@ for c,dictionary in enumerate(data['nodes']):
 
 
 #print(updatedcolors)
-keys = list(id_label.keys())
+#keys = list(id_label.keys())
+#for c,dictionary in enumerate(data['edges']):
+    #for i in range(len(keys)):
+        #if dictionary['source'] in keys[i]:
+            #print(True)
+            ##size_update = data['edges'][c]['size']*25
+            #data['edges'][c]['size'] = 10.0
+            #print(data['edges'][c]['size'])
+        #else:
+            #continue
 for c,dictionary in enumerate(data['edges']):
     for i in range(len(keys)):
-        if dictionary['source'] in keys[i]:
+        if dictionary['target'] in keys[i]:
             print(True)
             #size_update = data['edges'][c]['size']*25
             data['edges'][c]['size'] = 10.0
             print(data['edges'][c]['size'])
-        else:
-            continue
-for c,dictionary in enumerate(data['edges']):
-    for i in range(len(keys)):
-        if dictionary['target'] in keys[i]:
-            #size_update = data['edges'][c]['size']*25
-            data['edges'][c]['size'] = 10.0#size_update
         else:
             continue            
 json.dump(data,open('data.json','w'))
