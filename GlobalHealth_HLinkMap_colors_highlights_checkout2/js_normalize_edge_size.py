fe964c092a55bd9111ae31b347195f95d1a2cc03
joneill <jnoneill@ucdavis.edu>
2024-04-05 17:54:14 -0700

    overdue updates

--- GlobalHealth_HLinkMap_colors_highlights_checkout2/js_normalize_edge_size.py
+++ GlobalHealth_HLinkMap_colors_highlights_checkout2/js_normalize_edge_size.py
@@ -10,7 +10,7 @@ import random
 data = json.load(open('data.json','r'))
 json.dump(data,open('data.json_bak','w'))
 
-ghdbiomed= pickle.load(open('ListofGlobalHealthTokensOverlay.pickle','rb')) 
+hlink= pickle.load(open('hlinktokens.pickle','rb'))
 
 label_id = {}
 id_label = {}
@@ -19,12 +19,9 @@ data = json.load(open('data.json','r'))
 json.dump(data,open('data.json_bak','w'))
 i = range(1,10)
 for c,dictionary in enumerate(data['nodes']):
-    if dictionary['label'] in ghdbiomed:
+    if dictionary['label'] in hlink:
         label_id[dictionary['label']] = dictionary['id']
         id_label[dictionary['id']] = dictionary['label']
-        #color_ = dictionary['color'].split(',')
-        #color_[1] = '255'
-        #color = ",".join(color_)
 
     for i in range(10):
         if 'Health Link Research Cluster ' + str(i+1)  in dictionary['label']:
@@ -32,24 +29,13 @@ for c,dictionary in enumerate(data['nodes']):
             id_label[dictionary['id']] = dictionary['label']
 
 
-#print(updatedcolors)
 keys = list(id_label.keys())
 for c,dictionary in enumerate(data['edges']):
     if dictionary['source'] in keys:
         print(True)
-        ###size_update = data['edges'][c]['size']*25
         data['edges'][c]['size'] = 10.0
         print(data['edges'][c]['size'])
     else:
         continue
 
-#for c,dictionary in enumerate(data['edges']):
-    #for i in range(len(keys)):
-        #if dictionary['target'] not in keys[i]:
-            #print(True)
-            ##size_update = data['edges'][c]['size']*25
-            #data['edges'][c]['size'] = 10.0
-            #print(data['edges'][c]['size'])
-        #else:
-            #continue            
 json.dump(data,open('data.json','w'))
